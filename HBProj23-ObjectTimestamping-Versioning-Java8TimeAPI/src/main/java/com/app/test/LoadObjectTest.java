93cc15e1e6df14945d3320133f586196dd638e69
SandipCodes <sandipsh321@gmail.com>
2019-09-18 10:50:30 +0530

    object timestamping and versioning using java8 Time Api

---
+++ HBProj23-ObjectTimestamping-Versioning-Java8TimeAPI/src/main/java/com/app/test/LoadObjectTest.java
@@ -0,0 +1,61 @@
+package com.app.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.app.entity.StockInfo;
+import com.app.utils.HibernateUtil;
+
+public class LoadObjectTest {
+
+	public static void main(String[] args) {
+
+		Session ses = null;
+		StockInfo stockInfo=null;
+		Transaction tx = null;
+		boolean flag = false;
+
+		// get session
+		ses = HibernateUtil.getSession();
+		// load object and then update data
+		stockInfo = ses.load(StockInfo.class, 17);
+
+		if (stockInfo != null) {
+
+			// setting new value to update
+                stockInfo.setPrice(6500);
+			  
+			try {
+				
+				tx = ses.beginTransaction();
+                ses.update(stockInfo);
+                flag=true;
+			} catch (HibernateException he) {
+				he.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+
+			} finally {
+
+				if (flag) {
+					tx.commit();
+					System.out.println("Object updated Successfully..");
+				} else {
+					tx.rollback();
+
+				}
+				// close session
+				HibernateUtil.closeSession(ses);
+				// close SessionFactory
+				HibernateUtil.closeFactory();
+
+			} // finally
+
+		} // if
+		else {
+			System.out.println("OBJECT NOT FOUND..");
+		}
+	}// main
+
+}// class
---
+++ HBProj23-ObjectTimestamping-Versioning-Java8TimeAPI/src/main/java/com/app/test/SaveObjectTest.java
@@ -0,0 +1,65 @@
+package com.app.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.app.entity.StockInfo;
+import com.app.utils.HibernateUtil;
+
+public class SaveObjectTest {
+
+	public static void main(String[] args) {
+		
+		Session ses=null;
+		StockInfo stockInfo=null;
+		Transaction tx=null;
+		boolean flag=false;
+		
+		//get session 
+		ses=HibernateUtil.getSession();
+		//create object
+	
+		 stockInfo=new StockInfo();
+		 
+	     	
+		
+		try {
+			tx=ses.beginTransaction();
+		     //set values for entity class object
+			stockInfo.setCompany("RIL");
+			stockInfo.setPrice(3600);
+			stockInfo.setStockName("RILSTOCK");
+		
+			ses.save(stockInfo);
+			ses.flush();
+			flag=true;
+		}
+		catch(HibernateException he) {
+			he.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+	
+		}
+		finally {
+		
+			  if(flag) {
+				  
+				  tx.commit();
+				  System.out.println("Object updated Successfully..");
+			  }
+			  else {
+				  tx.rollback();
+				  System.out.println("Obejct not updated..");
+			  }
+			//close session
+			HibernateUtil.closeSession(ses);
+			//close SessionFactory
+			HibernateUtil.closeFactory();
+			
+		}//finally
+		
+	}//main
+
+}//class
