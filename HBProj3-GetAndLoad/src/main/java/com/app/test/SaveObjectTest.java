5a80058413256b28627e9002154152c50e77499b
SandipCodes <sandipsh321@gmail.com>
2019-09-18 10:53:31 +0530

    get and load

---
+++ HBProj3-GetAndLoad/src/main/java/com/app/test/SaveObjectTest.java
@@ -0,0 +1,76 @@
+package com.app.test;
+
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.app.entity.InsurancePolicy;
+
+public class SaveObjectTest {
+
+	public static void main(String[] args) {
+	
+		Configuration cfg=null;
+		SessionFactory factory=null;
+		Session session=null;
+		InsurancePolicy policy=null;
+		
+		Transaction tx=null;
+		boolean flag=false;
+		
+		//bootstrap hibernate
+		cfg=new Configuration();
+		
+		//load cfgs and prepare inmem meta data
+		cfg=cfg.configure("/com/app/cfgs/hibernate.cfg.xml");
+		
+		//build session factory
+		factory=cfg.buildSessionFactory();
+		
+		//open Session
+		session=factory.openSession();
+		
+		//create ENtity Class obj
+		policy=new InsurancePolicy();
+		
+		policy.setPolicyId(10003);
+		policy.setPolicyHolderName("SAC");
+		policy.setPolicyName("JEEVAN  BEEMA");
+		policy.setAadharNo(789758l);
+		policy.setTenure(20);
+		
+		
+		try {
+		
+			 tx=session.beginTransaction();
+			 session.save(policy);
+		     flag=true;
+		}
+		catch(HibernateException he) {
+			he.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			
+			if(flag) {
+				tx.commit();
+				System.out.println("Object saved Successfully..");
+			}
+			else {
+				tx.rollback();
+				
+			}
+			
+			//closing session
+			session.close();
+			//closing factory
+			factory.close();
+		}//finally
+	}//main
+
+}//class
